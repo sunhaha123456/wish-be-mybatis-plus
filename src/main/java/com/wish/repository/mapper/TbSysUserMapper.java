365b3a6850174f63248a6027003f2bc144f6e97c
sunp <sunp@dcnoahark.com>
2020-12-18 17:13:38 +0800

    实现查询基本功能

--- src/main/java/com/wish/repository/mapper/TbSysUserMapper.java
+++ src/main/java/com/wish/repository/mapper/TbSysUserMapper.java
@@ -7,13 +7,18 @@ import com.wish.data.entity.TbSysUser;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
-import java.util.Map;
 
 public interface TbSysUserMapper extends BaseMapper<TbSysUser> {
+
+    // -------------------------------- 进行演示的例子 --------------------------------
 
     List<TbSysUser> selectByUnameAndPwd(@Param("loginName") String loginName, @Param("upwd") String upwd);
 
     IPage<TbSysUser> selectUserPage(Page<TbSysUser> page);
 
     IPage<TbSysUser> selectPageByRoleGroupId(Page<TbSysUser> page, @Param("roleGroupId") Long roleGroupId);
+
+    List<TbSysUser> selectLoginNameByRoleGroupId(@Param("roleGroupId") Long roleGroupId);
+
+    List<String> selectLoginNameByUserState(@Param("userState") Integer userState);
 }
